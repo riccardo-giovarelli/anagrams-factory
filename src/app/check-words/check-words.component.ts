0b58e2c7cfb684e850b3578bde2afb304f3ed728
Riccardo Giovarelli <riccardo.giovarelli@gmail.com>
2018-08-16 03:42:00 +0200

    feat: Add dimmer and other improvement

--- src/app/check-words/check-words.component.ts
+++ src/app/check-words/check-words.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { LoadDicService } from '../load-dic.service';
+import * as _ from 'underscore';
 
 @Component({
   selector: 'app-check-words',
@@ -15,33 +16,49 @@ export class CheckWordsComponent {
 
   constructor(private _loadDic: LoadDicService) { }
 
-  analizeList = (currentStrings: Array<string>): void => {
-    this.results = [];
-    const dictonary = this._loadDic.getDictionary();
-    const dictonaryArray = JSON.parse(dictonary);
+  analizeList = (currentStrings: Array<string>) => {
 
-    for (const currentString of currentStrings) {
-      for (const currentWord of dictonaryArray) {
-        if (currentWord.word.toLowerCase() === currentString.toLowerCase()) {
-          this.results.push(currentString.toLowerCase());
+    document.getElementById('overlay').style.display = 'block';
+    document.querySelector('div[id=\'main\']').setAttribute('class', 'loader');
+
+    _.defer(() => {
+      this.results = [];
+      const dictonary = this._loadDic.getDictionary();
+      const dictonaryArray = JSON.parse(dictonary);
+
+      for (const currentString of currentStrings) {
+        for (const currentWord of dictonaryArray) {
+          if (currentWord.word.toLowerCase() === currentString.toLowerCase()) {
+            this.results.push(currentString.toLowerCase());
+          }
         }
       }
-    }
 
-    console.log(this.results.length);
-
-    if (this.results.length === 0) {
-      // Hide all li element
-    }
-
-    loopAnagrams:
-    for (const currentString of currentStrings) {
-      for (const realWorld of this.results) {
-        if (realWorld.toLowerCase() === currentString.toLowerCase()) {
-          continue loopAnagrams;
-        }
+      switch (this.results.length === 0) {
+        case true:
+          const liElements = document.querySelectorAll('li[id^=\'li_\']');
+          for (let i = 0; i < liElements.length; ++i) {
+            liElements[i].setAttribute('style', 'display: none;');
+          }
+          break;
+        case false:
+          loopAnagrams:
+          for (const currentString of currentStrings) {
+            for (const realWorld of this.results) {
+              if (realWorld.toLowerCase() === currentString.toLowerCase()) {
+                continue loopAnagrams;
+              }
+            }
+            document.getElementById('li_' + currentString.toLowerCase()).style.display = 'none';
+          }
+          break;
       }
-      document.getElementById('li_' + currentString.toLowerCase()).style.display = 'none';
-    }
+      _.defer(() => {
+        setTimeout(() => {
+          document.getElementById('overlay').style.display = 'none';
+          document.querySelector('div[id=\'main\']').setAttribute('class', '');
+        }, 1000);
+      });
+    });
   }
 }
