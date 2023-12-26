c5e1e67e445fcf77a56456bae460f063db5488a2
Riccardo Giovarelli <riccardo.giovarelli@gmail.com>
2023-12-26 22:41:30 +0100

    [CHORE] Simple improvements for tests

--- backend/__tests__/api/anagram/anagram.test.ts
+++ backend/__tests__/api/anagram/anagram.test.ts
@@ -1,12 +1,10 @@
 import request from 'supertest';
 
-import app from '../../index';
-import { getFactorial } from '../../utils/math.lib';
-import { isJson } from '../../utils/tests.lib';
+import app from '../../../index';
+import { getFactorial } from '../../../utils/math.lib';
+import { isJson } from '../../../utils/tests.lib';
+import { testWord } from '../../utils/constants';
 import { AnagramResposneType } from './anagram.test.type';
-
-const testWord = 'rick';
-const testWordAnagramsCount = getFactorial(testWord.length);
 
 describe(`GET /api/anagram/make?text=${testWord}`, () => {
   it(`should return all the anagrams for the word "${testWord}"`, async () => {
@@ -20,7 +18,7 @@ describe(`GET /api/anagram/make?text=${testWord}`, () => {
         const resultsJson = JSON.parse(res.text);
         expect(resultsJson).toHaveProperty('data');
         expect(typeof resultsJson.data).toBe('object');
-        expect(resultsJson.data.length).toBe(testWordAnagramsCount);
+        expect(resultsJson.data.length).toBe(getFactorial(testWord.length));
       });
   });
 });
--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -1,16 +1,14 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    transform: {
-        '^.+\\.ts?$': 'ts-jest',
-    },
-    transformIgnorePatterns: ['<rootDir>/node_modules/'],
-    testMatch: [
-        "<rootDir>/tests/**/*.test.ts"
-    ],
-    verbose: true
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  testMatch: ['<rootDir>/__tests__/**/*.test.ts'],
+  verbose: true,
 };
 
 export default config;
