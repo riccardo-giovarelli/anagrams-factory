6277f7c0fee871a3664dc9fd10d7434261343d4f
Riccardo Giovarelli <riccardo.giovarelli@gmail.com>
2024-01-20 15:22:11 +0100

    [FEAT] New config file for Cypress made for Vite - React

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
+
+import customViteConfig from './vite.config';
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
+      viteConfig: customViteConfig,
     },
   },
 });
